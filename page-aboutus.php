0eccfe9908a1f1a5f06dbc64a2499397ce64918a
Taylor Durham <tdurham@sullivanbranding.com>
2012-08-30 11:30:54 -0500

    added sidebar callouts and search box to about us sidebar

--- page-aboutus.php
+++ page-aboutus.php
@@ -13,8 +13,46 @@ get_header(); ?>
 		<div id="primary" class="site-content about-us">
 			<div id="content" role="main">
                 <div id="sidebar">
+                
+                    <aside id="findAStore">
+                        <div class="findAStoreContainer">
+                            <div class="center">
+                                <img src="http://viking.sullivanbranding.com/wp-content/themes/Viking/images/findAStore.png" alt="Find A Store">
+                            </div>
+                            <form method="get">
+                                <label for="City">City</label>
+                                <input type="text" name="City">
+                                <span class="or">or</span>
+                                <label for="Zip">Zip</label>
+                                <input type="text" name="Zip">
+                                <label class="distance" for="Distance">Distance</label>
+                                <select class="distance">
+                                    <option>5 miles</option>
+                                    <option>10 miles</option>                                
+                                    <option>25 miles</option>
+                                    <option>50 miles</option>                                
+                                    <option>75 miles</option>
+                                    <option>100 miles</option>                                
+                                </select>
+                                <input id="findAStoreSubmit" type="submit" value="GO">
+                            </form>
+                        </div>
+                    </aside>
+                                
                     <div class="callout">
-                        <h3>Find A Class</h3>
+                        <h3>Promotions And Specials</h3>
+                        <div class="copy">
+                            <div class="image">
+                                <img src="http://viking.sullivanbranding.com/wp-content/themes/Viking/images/callout_specialsAndPromotions.jpg" alt="">
+                            </div>
+                            <div class="calloutButton">
+                                <a href="#link"><img src="<?php echo get_template_directory_uri(); ?>/images/calloutBtn_SeeMore.png" alt="See More" /></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="callout">
+                        <h3>Local Class Schedule</h3>
                         <div class="copy">
                             <div class="image">
                                 <img src="http://viking.sullivanbranding.com/wp-content/themes/Viking/images/callout_findAClass.jpg" alt="">
@@ -24,6 +62,7 @@ get_header(); ?>
                             </div>
                         </div>
                     </div>
+
                 </div>
                 
                 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
