569a0da0b1dfc53d5f71dd5181edff0982d16b81
Taylor Durham <tdurham@sullivanbranding.com>
2012-08-31 14:03:02 -0500

    Sew Savvy Savings Page Development

---
+++ content-sewsavvysavings.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template used for displaying page content in page-sewsavvysavings.php
+ *
+ * @package Viking
+ * @since Viking 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="entry-content">
+        <div class="coreContent">
+            <div class="copy">
+        		<?php the_content(); ?>
+            </div>
+        </div>
+	</div><!-- .entry-content -->
+</article><!-- #post-<?php the_ID(); ?> -->
---
+++ page-sewsavvysavings.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template Name: Sew Savvy Savings
+ *
+ * The template for Sew Savvy Savings Page
+ *
+ * @package Viking
+ * @since Viking 1.0
+ */
+
+get_header(); ?>
+
+		<div id="primary" class="site-content sew-savvy-savings">
+			<div id="content" role="main">
+                <div id="sidebar">
+                
+                </div>
+                
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', 'sewsavvysavings' ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+								
+			</div><!-- #content -->
+		</div><!-- #primary .site-content -->
+
+<?php get_sidebar('pre-footer'); ?>
+<?php get_footer(); ?>
