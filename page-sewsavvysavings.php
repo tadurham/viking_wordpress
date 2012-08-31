b6187cf81d02ec007cb695e3481aac426b90db36
Taylor Durham <tdurham@sullivanbranding.com>
2012-08-31 16:24:58 -0500

    SSS: sidebar content

--- page-sewsavvysavings.php
+++ page-sewsavvysavings.php
@@ -13,8 +13,26 @@ get_header(); ?>
 		<div id="primary" class="site-content sew-savvy-savings">    		
 			<div id="content" role="main">
                 <div id="sidebar">
-                
+                    <div class="checkMyPoints">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/sss_checkMyPoints.png" alt="Check My Points" />
+                        <form method="get">
+                            <div class="fields">
+                                <label for="Name">Name</label>
+                                <input type="text" name="Name">
+                                <span class="orGreen">or</span>
+                                <label for="Phone">Phone</label>
+                                <input type="text" name="Phone">
+                            </div>
+                            <div class="submit">
+                                <input type="submit" value="Log In">                                
+                            </div>
+                        </form>                    
+                    </div>
+                    <div class="faqView">
+                        <a href="/faq/"><img src="<?php echo get_template_directory_uri(); ?>/images/faqView.png" alt="" /></a>
+                    </div>
                 </div>
+
                 
 				<?php while ( have_posts() ) : the_post(); ?>
 
