d6070ff597b0c6bbfc6fa3722490f651354e9623
Robert Smith <rgsmith1980@gmail.com>
2012-08-31 16:30:03 -0500

    Wrapping the header graphic in a container

--- page-aboutus.php
+++ page-aboutus.php
@@ -10,7 +10,13 @@
 
 get_header(); ?>
 
-<?php if(has_post_thumbnail()): the_post_thumbnail(); endif; ?>
+<?php if(has_post_thumbnail()): ?>
+    <div class="aboutus-header-wrap">
+        <a href="/products/">
+<?php the_post_thumbnail(); ?>
+        </a>
+    </div>
+<?php endif; ?>
 		<div id="primary" class="site-content about-us">
 			<div id="content" role="main">
                 <div id="sidebar">
