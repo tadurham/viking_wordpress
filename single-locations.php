17e636b72ad7ae45a2f1fcd1cae6508dc2fee1af
Robert Smith <rgsmith1980@gmail.com>
2012-09-14 10:58:35 -0500

    Moved callouts into a separate file and updated templates accordingly to use it.

--- single-locations.php
+++ single-locations.php
@@ -78,53 +78,7 @@
                 	</div><!-- .entry-content -->
                 </article>
 
-                <div id="callouts">
-                    <div class="callout">
-                        <h3>Find A Class</h3>
-                        <div class="copy">
-                            <div class="image">
-                                <img src="<?php echo get_template_directory_uri(); ?>/images/callout_findAClass.jpg" alt="" />
-                            </div>
-                            <h4>See your local class schedule</h4>
-                            <p>Learn new techniques and tools to make your sewing the best it can be. Choose from a variety of embroidery, quilting, embroidery software and machine owners&rsquo; classes.</p>
-                            <div class="actionButton"><a class="ribbon" href="#link">See More</a></div>
-                        </div>
-                    </div>
-                    <div class="callout">
-                        <h3>Specials And Promotions</h3>
-                        <div class="copy">
-                            <div class="image">
-                                <img src="<?php echo get_template_directory_uri(); ?>/images/callout_specialsAndPromotions.jpg" alt="" />
-                            </div>
-                            <h4>Our latest specials and promotions</h4>
-                            <p>Be the first to know about current offers and coupons available at your local Viking Sewing Gallery or Ultimate Sewing Place store.</p>
-                            <div class="actionButton"><a class="ribbon" href="/specials-and-promotions/">See More</a></div>
-                        </div>
-                    </div>
-                    <div class="callout">
-                        <h3>Sew Savvy Savings</h3>
-                        <div class="copy">
-                            <div class="image">
-                                <img src="<?php echo get_template_directory_uri(); ?>/images/callout_sewSavvySavings.jpg" alt="" />
-                            </div>
-                            <h4>Learn about our Loyalty Program</h4>
-                            <p>Earn rewards points for every dollar you spend, and apply your rewards toward future purchases.</p>
-                            <div class="actionButton"><a class="ribbon" href="/sew-savvy-savings/">See More</a></div>
-                        </div>
-                    </div>
-                    <div class="callout last">
-                        <h3>Products</h3>
-                        <div class="copy">
-                            <div class="image">
-                                <img src="<?php echo get_template_directory_uri(); ?>/images/callout_products.jpg" alt="" />
-                            </div>
-                            <h4>See what our stores offer</h4>
-                            <p>Browse our complete line of sewing machines, embroidery machines, quilting machines and other great products.</p>
-                            <div class="actionButton"><a class="ribbon" href="/products/">See More</a></div>
-                        </div>
-                    </div>
-                    <div class="clearFix"></div>
-                </div> <!-- !#callouts -->
+                <?php get_template_part( 'content', 'callouts' ); ?>
 
 			</div><!-- #content -->
                     
